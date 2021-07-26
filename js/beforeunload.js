785ef496f8bf46c42fba50a8e4c3afbe9b8bb61a
pbscorp <78822378+pbscorp@users.noreply.github.com>
2021-07-25 23:47:43 -0400

    change message

--- js/beforeunload.js
+++ js/beforeunload.js
@@ -1,18 +1,18 @@
 function fncEnableSubmit() {
-    document.getElementById("submitButton").disabled=false;
+    document.getElementById("submitButton").disabled = false;
     g_blnFormHasUnsubmittedData = true;
 }
-function fncRemoveBeforeUnloadEvent (n_strDiv) {
+function fncRemoveBeforeUnloadEvent(n_strDiv) {
     g_blnFormHasUnsubmittedData = false;
 }
 window.onload = function () {
     g_blnFormHasUnsubmittedData = false;
-    document.getElementById("submitButton").disabled=true;
-    
+    document.getElementById("submitButton").disabled = true;
+
     document.getElementById("bodyID").addEventListener('change', fncEnableSubmit);
-    window.addEventListener("beforeunload", function(e) {
+    window.addEventListener("beforeunload", function (e) {
         if (g_blnFormHasUnsubmittedData) {
-            e.returnValue = " address incomplete";
+            e.returnValue = "changes will be lost";
         }
     });
 }
